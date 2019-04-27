80575bc28c1b17b6bb1b3d06522bfcf7a37550bc
Wagdy Navas <dinholelles@gmail.com>
2019-04-27 16:54:43 -0300

    Reader implementation

---
+++ src/main/java/com/wagdynavas/register/repositoty/ReaderRepository.java
@@ -0,0 +1,8 @@
+package com.wagdynavas.register.repositoty;
+
+import com.wagdynavas.register.domain.Reader;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReaderRepository extends JpaRepository<Reader,String> {
+
+}
